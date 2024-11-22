f7909075e2f5de262957ae813681e566ccce7a04
mhung <145217559+henrynguci@users.noreply.github.com>
2024-11-22 17:22:09 +0700

    Refactor/initproject (#7)
    * refactor: init project structure with backend and frontend setup
    
    * refactor: init project
    
    * refactor : add rules
    
    * fix : rules
    
    * refactor: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+};
