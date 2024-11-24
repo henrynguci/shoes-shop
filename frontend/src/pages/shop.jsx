9239800dc92999bde1817a8dbecdabbc90d8a4f1
QuanTran <tranminhquan.411@gmail.com>
2024-11-24 22:02:20 +0700

    project stolen from git

---
+++ frontend/src/pages/shop.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Cart from './cart.jsx';
+import Home from './Home.jsx';
+import ProductDetail from './productDetail.jsx';
+import Success from './success.jsx';
+import Fail from './failed.jsx';
+
+const Shop = () => {
+
+  return ( 
+    <>
+        <Header />
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path="/product/*" element={<ProductDetail />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/fail" element={<Fail />} />
+        </Routes>
+        <Footer/>                
+    </>
+
+  );
+};
+
+export default Shop;
